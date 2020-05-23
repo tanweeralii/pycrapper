f6368e40fa434257d5bcf7fc9fdfa34cfdb828ac
tanweeralii <tanweeralii908@gmail.com>
2020-05-23 23:54:05 +0530

    Added chrome driver extension

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
-import dryscrape
-from bs4 import BeautifulSoup
-session = dryscrape.Session()
-session.visit("https://twitter.com/explore/tabs/trending")
-response = session.body()
-soup = BeautifulSoup(response,features="lxml")
-print(soup)
+from selenium import webdriver
 
-
-#for i in soup.body:
-#    print(i)
-#    j = i.split('\\n')
-#print("Top Trendings in twitter are : \n")
-#for i in j:
-#    if 'data-trend-name=' in i:
-#        split1 = i.split('\"')
-#        split2 = split1[1].split('\\')
-#        print(split2[0])
+# Access to Twitter
+url = r'https://twitter.com'
+driver = webdriver.Chrome()
+driver.get(url)
