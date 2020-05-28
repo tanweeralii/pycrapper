ef23f87e58da54215af48c20e0e3c7c90e8de44b
tanweeralii <tanweeralii908@gmail.com>
2020-05-28 20:56:19 +0530

    Updated Selenium:

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://twitter.com/explore/tabs/tab_1')
+res = requests.get('https://twitter.com/explore/tabs/trending')
 soup = BeautifulSoup(res.text, 'html5lib')
 for i in soup.body:
     print(i)
-    j = i.split('\\n')
-print("Top Trendings in twitter are : \n")
-for i in j:
-    if 'data-trend-name=' in i:
-        split1 = i.split('\"')
-        split2 = split1[1].split('\\')
-        print(split2[0])
+    print('\n') 
+    #j = i.split('\\n')
+#print("Top Trendings in twitter are : \n")
+#for i in j:
+    #if 'data-trend-name=' in i:
+        #split1 = i.split('\"')
+        #split2 = split1[1].split('\\')
+        #print(split2[0])
--- test.py
+++ test.py
@@ -2,19 +2,20 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 chrome_options = Options()
-chrome_options.add_argument('headless')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--remote-debugging-port=9222')
 driver = webdriver.Chrome(executable_path='/home/tanweer/Desktop/github_repo/pycrapper/chromedriver',chrome_options=chrome_options)
-url = r'https://twitter.com/explore/tabs/trending'
-driver.get(url)
-time.sleep(3)
-print(driver.page_source)
+driver.get('https://www.thenewbieprojects.com/Login.html')
+#print(driver.page_source)
+print("Page Title is: %s" %driver.title)
+driver.quit()
+#time.sleep(10)
+#print(driver.execute_script("return document.body.innerHTML")
+#print(driver.page_source)
